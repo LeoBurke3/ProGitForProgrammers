72fc3b80496307dd33c3c80fbbad3b6f36658c47
Leo Burke <leoburke78@gmail.com>
2023-12-06 16:36:06 +0000

    Functioning nav bar with Asset and Employee page displaying employees and assets.

---
+++ ProGitForProgrammersProject2/ProGitForProgrammersProject2/HomePage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ProGitForProgrammersProject2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            this.InitializeComponent();
+        }
+        private void Add_Employee_Popup(object sender, RoutedEventArgs e)
+        {
+            Add_Employee_Form.IsOpen = true;
+        }
+        private void Add_Asset_Popup(object sender, RoutedEventArgs e)
+        {
+            Nav_Pop_Add_Asset.IsOpen = true;
+
+        }
+
+        private void Add_Asset_Click(object sender, RoutedEventArgs e)
+        {
+            Asset asset = new Asset
+            {
+                name = assetName.Text,
+                model = assetModel.Text,
+                manufacturer = assetManu.Text,
+                type = assetType.Text,
+                ipAddress = assetIP.Text
+            };
+            asset.addAsset(asset);
+        }
+
+        private void View_Employee_Popup(object sender, RoutedEventArgs e)
+        {
+            Nav_Pop_View_Employee.IsOpen = true;
+            Employee employee = new Employee();
+            StaffList.ItemsSource = employee.viewEmployee();
+
+        }
+        private void Add_Employee_Click(object sender, RoutedEventArgs e)
+        {
+            Employee employee = new Employee
+            {
+                firstName = employeeFirstname.Text,
+                surname = employeeSurname.Text,
+                email = employeeEmail.Text
+
+            };
+            employee.addEmployee(employee);
+        }
+        private void View_Asset_Popup(object sender, RoutedEventArgs e)
+        {
+            Nav_Pop_View_Asset.IsOpen = true;
+            Asset asset = new Asset();
+            AssetList.ItemsSource = asset.viewAsset();
+        }
+    }
+}
--- ProGitForProgrammersProject2/ProGitForProgrammersProject2/MainPage.xaml.cs
+++ ProGitForProgrammersProject2/ProGitForProgrammersProject2/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -35,50 +36,83 @@ namespace ProGitForProgrammersProject2
 
         private void Add_Employee_Popup(object sender, RoutedEventArgs e)
         {
-           Add_Employee_Form.IsOpen = true;
+           //Add_Employee_Form.IsOpen = true;
         }
         private void Add_Asset_Popup(object sender, RoutedEventArgs e)
         {
-            Nav_Pop_Add_Asset.IsOpen = true;
+            //Nav_Pop_Add_Asset.IsOpen = true;
             
         }
 
         private void Add_Asset_Click(object sender, RoutedEventArgs e)
         {
-            Asset asset = new Asset
-            {
-                name = assetName.Text,
-                model = assetModel.Text,
-                manufacturer = assetManu.Text,
-                type = assetType.Text,
-                ipAddress = assetIP.Text
-            };
-            asset.addAsset(asset);
+            //Asset asset = new Asset
+            //{
+            //    name = assetName.Text,
+            //    model = assetModel.Text,
+            //    manufacturer = assetManu.Text,
+            //    type = assetType.Text,
+            //    ipAddress = assetIP.Text
+            //};
+            //asset.addAsset(asset);
         }
 
         private void View_Employee_Popup(object sender, RoutedEventArgs e)
         {
-            Nav_Pop_View_Employee.IsOpen = true;
+            //Nav_Pop_View_Employee.IsOpen = true;
             Employee employee = new Employee();
-            StaffList.ItemsSource = employee.viewEmployee();
+            //StaffList.ItemsSource = employee.viewEmployee();
 
         }
         private void Add_Employee_Click(object sender, RoutedEventArgs e)
         {
-            Employee employee = new Employee
-            {
-                firstName = employeeFirstname.Text,
-                surname = employeeSurname.Text,
-                email = employeeEmail.Text
+            //Employee employee = new Employee
+            //{
+            //    firstName = employeeFirstname.Text,
+            //    surname = employeeSurname.Text,
+            //    email = employeeEmail.Text
            
-            };
-            employee.addEmployee(employee);
+            //};
+            //employee.addEmployee(employee);
         }
         private void View_Asset_Popup(object sender, RoutedEventArgs e)
         {
-            Nav_Pop_View_Asset.IsOpen = true;
+            //Nav_Pop_View_Asset.IsOpen = true;
             Asset asset = new Asset();
-            AssetList.ItemsSource = asset.viewAsset();
+            //AssetList.ItemsSource = asset.viewAsset();
+        }
+
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected)
+            {
+
+            }
+            else
+            {
+                NavigationViewItem item = args.SelectedItem as NavigationViewItem;
+                switch (item.Tag.ToString()) 
+                {
+                    case "employees":
+                        ContentFrame.Navigate(typeof(EmployeePage));
+                        break;
+                    case "assets":
+                        ContentFrame.Navigate(typeof(AssetPage));
+                        break;
+                    case "software_assets":
+                        ContentFrame.Navigate(typeof(SoftwareAssetPage));
+                        break;
+                    case "home":
+                        ContentFrame.Navigate (typeof(HomePage));
+                        break;
+
+                }
+            }
+        }
+
+        private void NavigationView_Loaded(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Navigate(typeof(HomePage));
         }
     }
 }
